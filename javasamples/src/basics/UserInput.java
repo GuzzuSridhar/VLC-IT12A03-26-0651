be3855f30a9aa31a07df564be7547141c45bce07
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-03-24 10:36:11 +0530

    add userinput

--- javasamples/src/basics/UserInput.java
+++ javasamples/src/basics/UserInput.java
@@ -1,7 +1,27 @@
 package basics;
 
+import java.util.Scanner;
+
 public class UserInput {
 public static void main(String[] args) {
+    Scanner scan  = new Scanner(System.in); // obtain scanner object
     
+    // reading a String from command line
+    System.out.print("Enter your name: ");
+    String name = scan.nextLine();
+    
+    // readin an Integer
+    System.out.print("Enter your Age: ");
+    int age = scan.nextInt();
+
+    // readin an double
+    System.out.print("Enter your Salary: ");
+    double sal = scan.nextDouble();
+    System.out.println("Welcome! " + name + " you are " + age + " years old," + " Earning $" + sal);
+    
+
+
+
+
 }
 }

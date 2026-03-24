c536c32f274e6d8fd4fdba2e4c317dfaae9957cf
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-03-24 14:32:47 +0530

    add Reverse

--- javasamples/src/exercises/ReverseOfString.java
+++ javasamples/src/exercises/ReverseOfString.java
@@ -1,7 +1,21 @@
 package exercises;
 
+import java.util.Scanner;
+
 public class ReverseOfString {
-public static void main(String[] args) {
-    
-}
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a String: ");
+        String in = input.next();
+
+        System.out.print("Reverse of " + in + " is "  );
+        // print reverse of String
+        for (int i = in.length()-1; i >=0; i--) {
+            System.out.print(in.charAt(i));
+        }
+        
+        input.close();
+
+        
+    }
 }

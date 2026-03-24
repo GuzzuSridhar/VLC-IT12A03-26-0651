28d34b275751e98fc45e0eda38d2df91e3d0cbdd
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-03-24 12:01:58 +0530

    change if

--- javasamples/src/basics/UserInput.java
+++ javasamples/src/basics/UserInput.java
@@ -5,21 +5,26 @@ import java.util.Scanner;
 public class UserInput {
 public static void main(String[] args) {
     Scanner scan  = new Scanner(System.in); // obtain scanner object
-    
-    // reading a String from command line
-    System.out.print("Enter your name: ");
-    String name = scan.nextLine();
-    
-    // readin an Integer
-    System.out.print("Enter your Age: ");
-    int age = scan.nextInt();
+      Scanner input = new Scanner(System.in);
+        System.out.print("Enter your salary: ");
+        int sal = input.nextInt();
+        if (sal > 1000 && sal < 2000) {
+            sal += 100;
+            System.out.println("you salary is : " + sal);
+        } else if (sal >= 2000) {
+            sal += 200;
+            System.out.println("you salary is : " + sal);
+        } else {
+            System.out.println("Else executed");
+        }
+        input.close();
 
-    // readin an double
-    System.out.print("Enter your Salary: ");
-    double sal = scan.nextDouble();
-    System.out.println("Welcome! " + name + " you are " + age + " years old," + " Earning $" + sal);
-
-    scan.close();
-
-    }
+        // ----------------------------------------
+        boolean b = true;
+        if (!b) {
+            System.out.println("something");
+        } else {
+            System.out.println("Nothing");
+        }
+}
 }

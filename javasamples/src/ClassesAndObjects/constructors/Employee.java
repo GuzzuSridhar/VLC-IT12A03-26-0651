a762f4d64721ecdb2ae07695f90e304d64739107
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-03-25 12:40:53 +0530

    add constructors

---
+++ javasamples/src/ClassesAndObjects/constructors/Employee.java
@@ -0,0 +1,48 @@
+package ClassesAndObjects.constructors;
+
+public class Employee {
+    String name;
+    int age;
+    float salary;
+    String dept;
+
+    // Constructors
+    // the name of a constructor will be the same as Class Name
+    // constructors help to create objects / instances
+    public Employee(String name, int age, float salary, String dept) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+        this.dept = dept;
+    }
+
+    public Employee(String name, int age, float salary) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public Employee(String name, int age, String dept) {
+        this.name = name;
+        this.age = age;
+        this.dept = dept;
+    }
+
+
+
+    public Employee() {
+    }
+
+    
+
+    
+// default constructor is implicitly present
+// when a constructor is defined in the class, the default constructor is no more implicitly present.
+// can add as many constructors as needed
+    
+
+}
---
+++ javasamples/src/ClassesAndObjects/constructors/Employees.java
@@ -0,0 +1,32 @@
+package ClassesAndObjects.constructors;
+
+public class Employees {
+    public static void main(String[] args) {
+        // using the default constructor
+        Employee emp = new Employee(); // calling the default constructor
+       emp.name = "xx";
+       emp.age = 34;
+       emp.salary = 998.66f;
+       emp.dept = "Sales";
+
+        Employee[] employees = new Employee[6];
+        // using the full loaded constructor
+        employees[0]  = new Employee("John", 30, 8879.99f, "sales");
+        employees[1]  = new Employee("Alex", 35, 8379.99f, "IT");
+        employees[2]  = new Employee("Bill", 40, 3879.99f, "sales");
+        // using Employee(String name, int age, String dept)
+        employees[3] = new Employee("Sara", 34,"Admin");
+        // using Employee(String name, int age, float salary)
+        employees[4] = new Employee("Bill", 23, 5645.09f);
+        // using Employee(String name)
+        employees[4] = new Employee("Bill");
+
+        for (Employee employee : employees) {
+            System.out.print("\n"+ employee.name+ " - " + employee.age +" - ");
+        }
+        
+    }
+}
+
+
+//  when a new keyword is used it calls a constructor

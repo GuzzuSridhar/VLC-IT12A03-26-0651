ff12146359c54f752300ef9d9fc17ca7d162d5c0
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-03-25 15:13:25 +0530

    add casting

---
+++ javasamples/src/basics/Casting.java
@@ -0,0 +1,20 @@
+package basics;
+
+public class Casting {
+    public static void main(String[] args) {
+        // 2 types of casting
+        // widening cast-> small -> large (safe)
+        // /narrow cast -> Large -> small (risky)
+
+        // wide cast
+        // 
+        short smallType = 10;
+        int bigType = smallType;
+
+        // Narrow cast
+        double largeType = 123888888888888999.456;
+        int small = (int)largeType; 
+        System.out.println(small);
+
+    }
+}

4294fbe19527e3499202f6afeeaca02dc9df3d95
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-03-25 12:07:48 +0530

    add static

--- javasamples/src/ClassesAndObjects/books/Book.java
+++ javasamples/src/ClassesAndObjects/books/Book.java
@@ -8,4 +8,12 @@ public class Book {
     int yop;
     String genre;
     int pageCount;
+    static int bookCount;
+    static String libName = "My Library";
 }
+
+
+//Static members are shared across all instances / objects of a class
+// Instance variables belong to Individual objects
+// Static variables can be used to keep track of class-wide information
+// static members can be accessed via the class name or the instance name
--- javasamples/src/ClassesAndObjects/books/BookArray.java
+++ javasamples/src/ClassesAndObjects/books/BookArray.java
@@ -13,24 +13,33 @@ public class BookArray {
                books[0].yop = 2001;
                books[0].genre = "Programming";
                books[0].pageCount = 298;
+              //  books[0].bookCount++;
+              Book.bookCount++;
+
         books[1] = new Book();
                books[1].title = "Python Fundamentals";
                books[1].author = "John";
                books[1].yop = 2001;
                books[1].genre = "Programming";
                books[1].pageCount = 298;
+              //  books[1].bookCount++;
+                Book.bookCount++;               
         books[2] = new Book();
                books[2].title = "Html Fundamentals";
                books[2].author = "John";
                books[2].yop = 2001;
                books[2].genre = "Programming";
                books[2].pageCount = 298;
+              //  books[2].bookCount++;
+               Book.bookCount++;               
         // System.out.println(Arrays.toString(books)); 
 
         // print the books
         for (Book book : books) {
             System.out.print("\n"+ book.title +" - "+book.author +" - "+book.yop +" - "+book.genre +" - "+ book.pageCount +" - ");
         }
+
+        System.out.println("\n"+Book.bookCount +" Books are available "+ "in "+ Book.libName );
 
 
      }

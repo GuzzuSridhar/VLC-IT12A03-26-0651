4f6ab6556d67860d789f83ea4bac10b3c59d2037
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-03-26 09:09:11 +0530

    add exercises

--- javasamples/src/methods/Exercises.java
+++ javasamples/src/methods/Exercises.java
@@ -9,7 +9,6 @@ public class Exercises {
             revStr += input.charAt(i);
         }
         return revStr + " is the reverse of " + input;
-
     }
 
     static boolean isPalindrome(String input){
@@ -19,9 +18,31 @@ public class Exercises {
         // else{
         //     return false;
         // }
-
         return Exercises.reverseOfString(input).equalsIgnoreCase(input) ? true : false;
 
+    }
+
+    static int vowelCount(String str){
+        // String newStr = str.
+        //     toLowerCase()
+        //     .replace("a", "")
+        //     .replace("e","")
+        //     .replace("i","")
+        //     .replace("o","")
+        //     .replace("u","");
+        // return str.length() - newStr.length();
+
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if(str.toLowerCase().charAt(i)=='a' || 
+            str.toLowerCase().charAt(i)=='e' ||
+            str.toLowerCase().charAt(i)=='i' || 
+            str.toLowerCase().charAt(i)=='o'|| 
+            str.toLowerCase().charAt(i)=='u'){
+                    count++;
+            }
+        }
+        return count;
     }
 
     public static void main(String[] args) {
@@ -35,14 +56,14 @@ public class Exercises {
         p.P.p(Exercises.reverseOfString(in));
         // System.out.println((Exercises.isPalindrome(in)) ? in + " is a Palindrome" : in + " is not a palindrome" );
         p.P.p((Exercises.isPalindrome(in)) ? in + " is a Palindrome" : in + " is not a palindrome" );
-        
+     
         // if(Exercises.isPalindrome(in)){
         //     System.out.println(in + " is a Palindrome");
         // }else{
         //     System.out.println(in + " is not a palindrome");
         // }
 
-    
+        p.P.p("There are " + Exercises.vowelCount(in) + " Vowels in " + in  );
 
         scan.close();
     }

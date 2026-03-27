30d6feb2ce761fad2661bb7c5bd404eb68169ac6
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-03-27 10:09:25 +0530

    add colors

---
+++ javasamples/src/Strings/Colors.java
@@ -0,0 +1,33 @@
+package Strings;
+
+public class Colors {
+public static void main(String[] args) {
+    System.out.println("------------Console Colors in Java------------");
+    System.out.println("\u001B[30m  This Black \u001B[0m");
+    System.out.println("\u001B[31m  This Red \u001B[0m");
+    System.out.println("\u001B[32m  This Green \u001B[0m");
+    System.out.println("\u001B[33m  This Yellow \u001B[0m");
+    System.out.println("\u001B[34m  This Blue \u001B[0m");
+    System.out.println("\u001B[35m  This Magenta \u001B[0m");
+    System.out.println("\u001B[36m  This Cyan \u001B[0m");
+    System.out.println("\u001B[37m  This White \u001B[0m");
+
+    System.out.println("\u001B[40m  This Background Black \u001B[0m");
+    System.out.println("\u001B[41m  This Background Red \u001B[0m");
+    System.out.println("\u001B[42m  This Background Green \u001B[0m");
+    System.out.println("\u001B[43m  This Background Yellow \u001B[0m");
+    System.out.println("\u001B[44m  This Background Blue \u001B[0m");
+    System.out.println("\u001B[45m  This Background Magenta \u001B[0m");
+    System.out.println("\u001B[46m  This Background Cyan \u001B[0m");
+    System.out.println("\u001B[47m  This Background White \u001B[0m");
+
+    System.out.println("\u001B[1m  This Bold Text \u001B[0m");
+    System.out.println("\u001B[3m  This Italic Text \u001B[0m");
+    System.out.println("\u001B[4m  This Underlined Text \u001B[0m");
+    // blink text may not be supported in all terminals
+    System.out.println("\u001B[5m  This Blinking Text \u001B[0m");
+    System.out.println("\u001B[7m  This Inverted Colors \u001B[0m");
+
+
+}
+}
